122c812c75362bcaba61620a8675386951fa3252
Rudi <gwdonkboi@gmail.com>
2023-02-05 06:53:56 +0700

    Add regexx class template
    Ide dari sulo

---
+++ 8x8.RegexRulesStrategy/RegexFilterRuleStrategy.cs
@@ -0,0 +1,21 @@
+﻿using _8x8.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _8x8.RegexRulesStrategy
+{
+    public class RegexFilterRuleStrategy : Disposable, IFilterRuleStrategy<string>
+    {
+        public IFilterRule FilterRule => throw new NotImplementedException();
+
+        public IEnumerable<string> Segments => throw new NotImplementedException();
+
+        public string Hash => throw new NotImplementedException();
+
+        public bool Equals([AllowNull] IFilterRuleStrategy<string> other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 8x8.RegexRulesStrategy/RegexStrategyWrapper.cs
@@ -0,0 +1,21 @@
+﻿using _8x8.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace _8x8.RegexRulesStrategy
+{
+    public class RegexStrategyWrapper : Disposable, IStrategyWrapper<string>
+    {
+        public IFilterRuleStrategy<string> FilterRuleStrategy => throw new NotImplementedException();
+
+        public int RuleId => throw new NotImplementedException();
+
+        public int Priority => throw new NotImplementedException();
+
+        public IStrategy Strategy => throw new NotImplementedException();
+
+        public IEnumerable<string> Segments => throw new NotImplementedException();
+
+        public string Hash => throw new NotImplementedException();
+    }
+}
